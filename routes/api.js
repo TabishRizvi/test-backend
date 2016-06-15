fd0daf4a7d0ab12e066444dacebb2b74e35d1c71
Tabish Rizvi <tabish.haidar@clicklabs.in>
2016-06-15 17:27:41 +0530

    update pic 2

--- routes/api.js
+++ routes/api.js
@@ -20,9 +20,9 @@ router.post('/login',controllers.api.common.LoginCtrl);
 
 router.delete('/logout',controllers.api.user.LogoutCtrl);
 
-router.get('/profile-view',controllers.api.user.ProfileViewCtrl);
+router.get('/profile/view',controllers.api.user.ProfileViewCtrl);
 
-router.put('/profile-update',controllers.api.user.ProfileUpdateCtrl);
+router.put('/profile/update',controllers.api.user.ProfileUpdateCtrl);
 
 
 router.put('/profile-pic/update',upload.single("profilePic"),controllers.api.user.ProfilePicUpdateCtrl);
